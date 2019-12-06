627a102b07e420138d9f968fc97d77dc369f8662
Matthias Holcombe <tmatthiash@gmail.com>
2019-12-06 13:18:53 -0600

    stupid merge thing

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-export const api_url = process.env.API_URL || "localhost";
-export const backend_port = process.env.PORT || 3000;
+// export const api_url = process.env.API_URL || "localhost";
+// export const backend_port = process.env.PORT || 3000;
 
-// export const api_url = process.env.API_URL || "dotvotrr.com";
-// export const backend_port = process.env.PORT || 80;
+export const api_url = process.env.API_URL || "dotvotrr.com";
+export const backend_port = process.env.PORT || 80;
 //import {api_url, backend_port} from "";
