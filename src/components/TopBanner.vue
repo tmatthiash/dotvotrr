de936857cb5e0d395f4edddee1b1ac0d84225d77
Matthias Holcombe <matthiash@localhost.localdomain>
2019-12-11 16:26:20 -0600

    home page update

--- src/components/TopBanner.vue
+++ src/components/TopBanner.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <b-jumbotron header="Dot Votrr" lead="Vote bettrr">
-      <p>Dot voting is a better way to make decisions as a group.</p>
+      <p>Start a dot vote without having to create an account. Just start a dot vote, share the room number with everyone and get voting.</p>
     </b-jumbotron>
   </div>
 </template>
@@ -18,5 +18,16 @@ export default {
   /* background-color: #231651; */
   background: #0d2c54;
   color: white;
+}
+@media (max-width: 350px) {
+  .jumbotron {
+    font-size: 80%;
+  }
+  h1 {
+    font-size: 3rem;
+  }
+}
+p {
+  margin: 1rem;
 }
 </style>
