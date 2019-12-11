01b19104e826ce9f01dbe6523ad166d008b8290a
Matthias Holcombe <tmatthiash@gmail.com>
2019-12-11 13:01:15 -0600

    make app component scale

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,31 @@
 <template>
-  <div id="app">
+  <div id="app" :style="`height: ${innerHeight}px`">
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      innerHeight: document.documentElement.clientHeight
+    };
+  },
+  methods: {
+    setHeight() {
+      this.innerHeight = document.documentElement.clientHeight;
+    }
+  },
+  mounted() {
+    this.$nextTick(function() {
+      window.addEventListener("resize", this.setHeight);
+    });
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.setHeight);
+  }
+};
+</script>
 
 <style>
 #app {
@@ -13,15 +36,11 @@
   color: #2c3e50;
   /* margin-top:20px; */
   /* padding-top: 20px; */
-  min-height: 100vh;  
+  min-height: 100vh;
   width: 100vw;
   /* background-color: orange; */
   background: rgb(255, 165, 0);
-  background: linear-gradient(
-    135deg,
-    rgba(255, 165, 0, 1) 45%,
-    #ff6400 100%
-  );
+  background: linear-gradient(135deg, rgba(255, 165, 0, 1) 45%, #ff6400 100%);
 }
 
 #nav {
