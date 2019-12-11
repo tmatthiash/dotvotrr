564835069bc251a45b914799597556de2cd1a8ef
EC2 Default User <ec2-user@ip-172-31-8-90.us-east-2.compute.internal>
2019-12-11 19:06:46 +0000

    remove min-height from app.vue

--- src/App.vue
+++ src/App.vue
@@ -36,7 +36,6 @@ export default {
   color: #2c3e50;
   /* margin-top:20px; */
   /* padding-top: 20px; */
-  min-height: 100vh;
   width: 100vw;
   /* background-color: orange; */
   background: rgb(255, 165, 0);
